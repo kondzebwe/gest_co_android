8b5c89e29cba7246a995cf3ade8b5f0e707e8ecb
kondzebwe <samir.chab23@gmail.com>
2023-06-21 14:27:29 +0200

    Mise a jour du theme de l'application

--- app/src/main/java/fr/doranco/flash/MainActivity.java
+++ app/src/main/java/fr/doranco/flash/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SpecialiteDatabaseTest test = new SpecialiteDatabaseTest(this);
-        test.start();
+
     }
 }
